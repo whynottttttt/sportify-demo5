41e0ba06fde8630a9e993767586e935620554823
whynottttttt <ekgus367@gmail.com>
2025-06-03 02:17:26 +0900

    first commit2

--- src/pages/PlaylistDetailPage/PlaylistDetailPage.tsx
+++ src/pages/PlaylistDetailPage/PlaylistDetailPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const PlaylistDetailPage = () => {
-  return (
-    <div>PlaylistDetailPage</div>
-  )
+    return (
+        <div>PlaylistDetailPage</div>
+    )
 }
 
 export default PlaylistDetailPage
