146d2ecad74cdeae8b5033abd85790140ba788ef
whynottttttt <ekgus367@gmail.com>
2025-06-07 22:53:46 +0900

    login3

--- src/configs/authConfig.ts
+++ src/configs/authConfig.ts
@@ -1,3 +1,3 @@
 export const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_SECRET_ID;
 export const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-export const REDIRECT_URI = process.env.REACT_APP_SPOTIFY_REDIRECT_URI || "http://127.0.0.1:3000/callback";
+export const REDIRECT_URI = process.env.REACT_APP_SPOTIFY_REDIRECT_URI || "https://sportify-demo3.vercel.app/callback";
